3da1e4450014e28ac43d6951f7ed5a6d21154506
Nəbizadə Kənan Namiq <NXbi_av4i@itstep.edu.az>
2021-07-13 12:34:59 +0400

    Add project files.

---
+++ Bank/User1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bank
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Card CreditCard { get; set; }
+        public User[] user { get; set; }
+        public User()
+        {
+        }
+        
+        void show()
+        {
+            Console.WriteLine("=============User Info===========");
+            Console.WriteLine($"Name : ");
+        }
+
+    }
+}
